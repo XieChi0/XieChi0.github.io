be586da91579cc9b9e0df60d84770ebadc19a57c
xchi <baiseliuxing@126.com>
2026-04-25 10:48:30 +0800

    feat: 支持配置banner的各项，包括高度，模糊度

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,12 +6,15 @@ export const LIGHT_MODE = "light",
 export const DEFAULT_THEME = AUTO_MODE;
 
 // Banner height unit: vh
-export const BANNER_HEIGHT = 35;
-export const BANNER_HEIGHT_EXTEND = 30;
-export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
+export const BANNER_HEIGHT = 35;		//非首页的 banner 高度
+export const BANNER_HEIGHT_EXTEND = 15;		//向上延伸的banner高度
+export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;		//首页的 banner 高度
+	// 首页的banner高度是二者之和，所以调大任意一个都会让首页变大，但是非首页的banner高度由banner_height决定
+export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 3.5;		//主内容区域与banner重叠的高度(单位：rem)
+export const BANNER_BLUR_HEIGHT = 3.5;		//banner底部模糊渐变的高度(单位：rem)
 
-// The height the main panel overlaps the banner, unit: rem
-export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 3.5;
+
+
 
 // Page width: rem
-export const PAGE_WIDTH = 75;
+export const PAGE_WIDTH = 75;		//页面宽度
